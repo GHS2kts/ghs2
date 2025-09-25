52dc316769f4f66b813b3600ee81e692d7d03092
GHS2kts <engr.khurram44@gmail.com>
2025-09-25 11:58:50 +0500

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  updateDoc,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBK5n_xfpyPpXB8YOa5_fs53ciLpRoC5lI",
+  authDomain: "ghs-dashboard.firebaseapp.com",
+  projectId: "ghs-dashboard",
+  storageBucket: "ghs-dashboard.firebasestorage.app",
+  messagingSenderId: "692567259749",
+  appId: "1:692567259749:web:4ef59b0ad8ba554d9de48c",
+  measurementId: "G-P5DP6W67DH"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+window.db = db;
