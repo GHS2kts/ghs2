55fe4d31852f47d83f2f0a280bb43b0bd75d0fb5
GHS2kts <engr.khurram44@gmail.com>
2025-09-27 09:50:40 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,20 +4,45 @@ const urlsToCache = [
   '/index.html',
   '/css/styles.css',
   '/assets/js/index.js',
+  '/assets/js/display.js',
   '/assets/bg-morning.jpg',
   '/assets/bg-afternoon.jpg',
   '/assets/bg-evening.jpg',
-  '/assets/qr-facebook-ghs.png'
+  '/assets/qr-facebook-ghs.png',
+  '/pages/display.html',
+  '/pages/login.html',
+  '/pages/teacher-portal.html',
+  '/pages/admin.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('📦 Caching dashboard assets');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('🧹 Removing old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
